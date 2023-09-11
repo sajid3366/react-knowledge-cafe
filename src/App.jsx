cee96b58447e94113fc04de76d67022f860eef68
sajid360 <ahteshamsajid8@gmail.com>
2023-09-12 00:04:11 +0600

    knowledge cafe

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-5xl bg-red-300'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
